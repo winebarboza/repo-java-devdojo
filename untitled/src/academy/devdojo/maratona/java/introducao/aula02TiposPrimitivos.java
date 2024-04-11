99a74101baaeb91c937ef626aff959d8285efa4b
winebarboza <wine.barbosa@gmail.com>
2024-04-10 21:13:52 -0300

    tipos primitivos em Java

---
+++ untitled/src/academy/devdojo/maratona/java/introducao/aula02TiposPrimitivos.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratona.java.introducao;
+
+public class aula02TiposPrimitivos {
+    public static void main(String[]args){
+        //int, double, float, char, byte, long, short, boolean
+        int numeroInteiro = 18;
+        long numeroGrande = 10000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2000;
+        short idadeShort = 18;
+        char caractere = '\u0041' ;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        byte idadeByte = 18;
+    }
+}
